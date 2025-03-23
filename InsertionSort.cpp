a2c4e2f7d6fbe4a8b06d7d169ffeefb9f01adcd0
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id.>
2025-03-23 13:53:10 +0700

    perulangan for untuk menyimpan data

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -24,4 +24,10 @@ cout << endl;
 cout << "======================" << endl;
 cout << "masukkan element Array" << endl;
 cout << "======================" << endl;
+
+for (int i = 0; i < n; i++)
+{
+    cout << "data ke-" << (i + 1) << ": ";
+    cin >> arr[i];
+}
 }
